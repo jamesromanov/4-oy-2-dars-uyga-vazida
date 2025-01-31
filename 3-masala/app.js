e597d5f700aabc60266b19677471fa35ae1092bf
James romanov <qahramonovjmashid1@gmail.com>
2025-01-31 09:49:22 +0500

    donee

--- 3-masala/app.js
+++ 3-masala/app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const os = require("os");
+const { log } = require("console");
 
 //shu masalada soralganidek qilish uchun shunchaki fayl nomi directoryda mavjud
 // bolmasa fs ozi fayl yaratib ichiga yozib qoyar ekan shu usulda foydalandim!
@@ -10,5 +11,7 @@ console.log(fs.readFileSync("./hello.txt", "utf-8"));
 
 let checkIfTrue = path.join(__dirname, "hello.txt");
 console.log(os.cpus());
+console.log(os.userInfo());
+console.log(os.arch());
 
 console.log(checkIfTrue);
